af48f0e444fe804547b672d960bf89299debfd7f
Unknown <stockmicah@gmail.com>
2019-01-30 10:28:35 -0700

    Fixed a lot of things, figured stuff out
    I'm happy with development right now.

--- Ordventure/game.js
+++ Ordventure/game.js
@@ -18,14 +18,44 @@ alert("The wind is blowing, and it's cold.");
 alert("The sun is just setting, but you just woke up. You are standing in a forest, surrounded by tall pine trees.");
 alert("There is a path that leads north.");
 
-var action = prompt("Where shall you go? You can use the commands North, East, South, West, or just the letters N E S W.").toLowerCase
+while (1==1){
+var action = prompt("Where shall you go? You can use the commands North, East, South, West, or just the letters N E S W.").toLowerCase();
+
     if(action == "north" || action == "n") {
-   alert("yeet");
+   alert("You begin on your way, hurrying through the night.");
+        break;
     }
     else if(action == "look" || action == "l")
     {
         alert(look);
+    } else if (action == "") {
+        alert("You forgot to type something genius.")
+    } else if (action == "west" || action == "w" || action == "east" || action == "e" || action == "south" || action == "s") {
+        alert("There's nothing but trees in that direction.")
+    } else {
+        alert("I don't understand your nonsense.");
     }
-    alert("The path goes on for about a mile it seems. Your eyes strain to see in the growing darkness. You don't feel like you are in danger though.")
+}
+    alert("The path goes on for about a mile it seems. Your eyes strain to see in the growing darkness. You don't feel like you are in danger though.");
+    alert("In the distance, you see lights. You couldn't see them before, as it is darker now than it was then.");
+    
+    while (1==1) {
+        
+        var action = prompt("You reach the gates. You don't immediatly spot any guards, but they may just be hiding in the dark").toLowerCase();
+        
+        if(action == "enter" || action == "go inside") {
+            alert("You walk inside the gates, still not spotting anyone. You hope no one hostile has spotted you.");
+            break;
+        } else if (action == "open gate" || action == "open gates" || action == "unlock gates" || action == "unlock gate") {
+            alert("The gates are already open Stevie Wonder.");
+            alert("You walk inside.");
+            break;
+        } else if (action == "") {
+            alert("You hecking doughnut, you didn't type anything.")
+        } else {
+            alert("I don't know what the hecc you're saying.");
+        }
+    }
+    
     }
 }
